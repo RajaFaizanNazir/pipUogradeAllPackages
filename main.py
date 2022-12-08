fda83db3148c3d89d9910c787112f8a04521a058
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-12-08 21:07:04 +0500

    initial

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("pip list > list.txt")
+
+Lines = open('list.txt', 'r').readlines()
+Lines = [i.split()[0] for i in Lines][2:]
+for i in Lines:
+    os.system("pip install --upgrade " + i)
