50362e61e9eefc6dc4bf603925de44f964ca5cb1
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-12-09 12:55:06 +0500

    pip upgrade

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
+os.system("pip install --upgrade pip")
 os.system("pip list > list.txt")
-
 Lines = open('list.txt', 'r').readlines()
 Lines = [i.split()[0] for i in Lines][2:]
 for i in Lines:
